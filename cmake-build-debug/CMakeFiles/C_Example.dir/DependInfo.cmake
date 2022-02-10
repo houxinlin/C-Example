f11572bc2dc19214920584b71a1672f3536d6a63
2606710413@qq.com <2606710413@qq.com>
2022-02-11 04:44:26 +0800

    add

--- cmake-build-debug/CMakeFiles/C_Example.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/C_Example.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/HouXinLin/project/c/C-Example/example/ip地址.c" "/home/HouXinLin/project/c/C-Example/cmake-build-debug/CMakeFiles/C_Example.dir/example/ip地址.c.o"
+  "/home/HouXinLin/project/c/C-Example/example/遍历目录.c" "/home/HouXinLin/project/c/C-Example/cmake-build-debug/CMakeFiles/C_Example.dir/example/遍历目录.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/C_Example.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/C_Example.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/C_Example.dir/example/ip地址.c.o"
+  "CMakeFiles/C_Example.dir/example/遍历目录.c.o"
   "C_Example"
   "C_Example.pdb"
 )
